f0ab670991f77a828a5b8b662c241b80ace74f34
neoray12 <neoray12@hotmail.com>
2020-09-05 06:36:52 +0000

    Adding comments

--- srv/server.js
+++ srv/server.js
@@ -3,6 +3,8 @@
 const cds = require("@sap/cds");
 const proxy = require("@sap/cds-odata-v2-adapter-proxy");
 
+// Serve OData v2 Proxy
+
 cds.on("bootstrap", app => app.use(proxy()));
 
 module.exports = cds.server;
